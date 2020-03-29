182a4fa0aca3fd11d4cb2a37abf4f3bc8affbb37
Will_Laptop <willszkwarek@gmail.com>
2020-03-28 20:39:58 -0500

    Make pixels randomly colored

--- canvas.js
+++ canvas.js
@@ -1,5 +1,12 @@
+function randRGB() {
+  r = Math.floor(Math.random() * 256);
+  g = Math.floor(Math.random() * 256);
+  b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`
+}
+
 for(let i = 0; i < 10; i++) {
   for (let j = 0; j < 10; j++) {
-    document.getElementById('canvas').innerHTML += `<div class="pixel"></div>`;
+    document.getElementById('canvas').innerHTML += `<div class="pixel" style="background-color:${randRGB()}"></div>`;
   }
 }
